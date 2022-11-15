7bcabea1a96648aeb5300d92fe252b38b2212885
djanloo <zzibecu@gmail.com>
2022-11-15 16:03:26 +0100

    made installable

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='serverstd',
+      version='1.0',
+      description='Server standard output redirect utility',
+      packages=['serverstd'],
+     )
